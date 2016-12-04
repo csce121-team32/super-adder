4bb7f238efcd07554b248bb73d749fb9fee942e2
felixj930 <felixj930@tamu.edu>
2016-12-04 13:55:06 -0600

    Update HS_list_manip.h
    changed so we can display the players score after the top 5 scores

--- scores/HS_list_manip.h
+++ scores/HS_list_manip.h
@@ -12,13 +12,14 @@ vector<Player> sort_scores(int difficulty, vector<Player> Players = load_file_sc
 //---------------------------------------
 
 //This function is used to add a new player to high score file
-void update_scores(string p_initials = "", int p_score = 0, int p_difficulty = 0, vector<Player> Players = load_file_scores());
+//call it with (initials, score, difficulty)
+void update_scores(string p_initials = "", double p_score = 0, int p_difficulty = 0, vector<Player> Players = load_file_scores());
 //------------------------------------------------------------
 
-//This part shows high scores for a certain difficulty if you leave second 
-//Parameter blank
-//If you include the initials then it will show initials after the high scores
-void display_scores(int difficulty, string initials = "");
+//(difficulty) top 5 high scores for that difficulty
+//(difficulty,initials) top 5 followed by players initials with ??? score
+//(difficulty, initials, score) top 5 then players initials and their score
+void display_scores(int difficulty, string initials = "", double score = -9999.0);
 //------------------------------------------------------------------------
 
 #endif
